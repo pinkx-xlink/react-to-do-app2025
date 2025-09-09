1a467a1cdb57097e791e3951a4e69fc449255840
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-09 17:52:45 -0400

    Create 7 dynamic buttons; one for each day of the week, plus the date

--- to-do/src/components/DateSidebar.jsx
+++ to-do/src/components/DateSidebar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DateSidebar = () => {
+
+  const [days] = useState(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+  const today = new Date();
+
   return (
+    <>
     <div>DateSidebar</div>
+    <div className='days__container'>
+        {days.map((day, index) => {
+            const date = new Date(today);
+            date.setDate(today.getDate() + index);
+            return (
+                <button key={day}>
+                    {day} - {date.toLocaleDateString()}
+                </button>
+            );
+        })}
+    </div>
+    </>
   )
 }
 
