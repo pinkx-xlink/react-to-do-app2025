fcbe354bfd072b51e3e0a7a00d1f9867ac70b084
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-11 12:33:39 -0400

    Add code that adds another task when Submit button is clicked; currently only adds the task, not the due date or other info, and appends the <ul> element to the header (not ideal)

--- to-do/src/components/NewToDoPopup.jsx
+++ to-do/src/components/NewToDoPopup.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import ToDoItem from './ToDoItem';
 
-const handleSubmit = () => {
-    console.log('ksdnkjn')
-    return (
-      <ToDoItem />
-    )
-  }
-  
+let nextId = 0;
+
 const NewToDoPopup = () => {
-  
+  const [task, setTask] = useState('');
+  const [artists, setArtists] = useState([]);
+
   return (
     <div>NewToDoPopup
          <h4>Popup - GeeksforGeeks</h4>
@@ -20,7 +17,14 @@ const NewToDoPopup = () => {
                 position="right center">
                  <form id="myForm">
                     <label for="task">Task:</label>
-                    <input type="text" id="task" name="task" placeholder="Enter the task" required />
+                    <input 
+                    type="text" 
+                    id="task" 
+                    name="task" 
+                    placeholder="Enter the task" 
+                    required 
+                    onChange={e => setTask(e.target.value)}
+                    />
                     <br/>
                     <label for="project">Project:</label>
                     <input type="text" id="project" name="project" placeholder="MAKE THIS A DROPDOWN" required />
@@ -31,9 +35,22 @@ const NewToDoPopup = () => {
                     <label for="dueDate">Due Date:</label>
                     <input type="date" id="dueDate" name="dueDate" placeholder="Enter the due date" required />
                     <br/>
-                    <button type="button" id="submitButton" onSubmit={handleSubmit}>Submit</button>
+                    <button 
+                    type="button" 
+                    id="submitButton" 
+                    onClick={() => {
+                        setArtists([
+                            ...artists,
+                            { id: nextId++, task: task }
+                        ]);
+                    }}>Submit</button>
                 </form>
             </Popup>
+            <ul>
+                {artists.map(artist => (
+                    <li key={artist.id}>{artist.task}</li>
+                ))}
+            </ul>
     </div>
   )
 }
